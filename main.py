fc419a88700b4f1cd027baaaa8cee959290d0d05
jgaik <jakub.gaik@gmail.com>
2019-09-15 11:18:05 +0200

    Changes to be xbee network connection function v0: 	modified:   main.py 	modified:   robot.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import server
 import tasks
 
 def main():
-    client = server.Client()
-    with client:
-        while True:
-            client.read(server.READ_MARKERS)
+	client = server.Client()
+	with client:
+		while True:
+			client.read(server.READ_MARKERS)
 
 if __name__ == "__main__":
-    main()
+	main()
--- robot.py
+++ robot.py
@@ -1,16 +1,47 @@
+import time
 import digi.xbee.devices as xb
 
+BROADCAST = -1
+
 class Robot:
-    xbeePortnum = "/dev/ttyUSB0"
-    xbeeBaudrate = "9600"
+  xbPortnum = "/dev/ttyUSB0"
+  xbBaudrate = "9600"
+  xbNetDiscoveryTimeOut = 10
 
-    def __init__(self, portnum = xbeePortnum, baudrate = xbeeBaudrate):
-        self.xbeeBaudrate = baudrate
-        self.xbeePortnum = portnum
-        self.xbeeDevice = xb.XBeeDevice(self.xbeePortnum, self.xbeeBaudrate)
-    
-    def __enter__(self):
-        self.xbeeDevice.open()
+  def __init__(self, portnum = xbPortnum, baudrate = xbBaudrate):
+    self.xbBaudrate = baudrate
+    self.xbPortnum = portnum
+    self.xbDevice = xb.XBeeDevice(self.xbPortnum, self.xbBaudrate)
+    self.xbRemotes = None
 
-    def __exit__(self, _, __, ___):
-        self.xbeeDevice.close()
+    self.robotNames = {}
+    for idx in range(0, 1):
+      self.robotNames[str(idx)] = f"Robot {idx}"
+  
+  def __enter__(self):
+    print("[Xbee network]: Initialising..")
+    self.xbDevice.open()
+    self.xbNet = self.xbDevice.get_network()
+    self.xbNet.set_discovery_timeout(self.xbNetDiscoveryTimeOut)
+    self.xbNet.start_discovery_process()
+    while self.xbNet.is_discovery_running():
+      time.sleep(0.5)
+      print(".")
+    self.xbRemotes = self.xbNet.get_devices()
+    print(f"[Xbee Network]: Initialisation finished.")
+    print(f"[Xbee Network]: {len(self.xbRemotes)} remote devices found.")
+
+  def __exit__(self, _, __, ___):
+    self.xbDevice.close()
+
+  def sendData(self, data, id = BROADCAST):
+    if id == BROADCAST:
+      self.xbDevice.send_data_broadcast(data)
+    elif str(id) not in self.robotNames:
+      remote = self.xbNet.get_device_by_node_id(self.robotNames[str(id)])
+      if remote is not None:
+        self.xbDevice.send_data_async(remote, data)
+      else:
+        print(f"[Xbee Network]: {self.robotNames[str(id)]} is not detected.")
+    else:
+      print(f"[Xbee Network]: Unknown id ({id}).")
