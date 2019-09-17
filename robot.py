b708e899818984b888dd9746e0be34abe5665324
jgaik <jakub.gaik@gmail.com>
2019-09-17 14:16:29 +0200

    Added update network function and general readability 	modified:   robot.py

--- robot.py
+++ robot.py
@@ -1,9 +1,10 @@
 import time
 import digi.xbee.devices as xb
 
+ROBOTNAME_IDX = (0, 0)
 BROADCAST = -1
 
-class Robot:
+class RobotNetwork:
   xbPortnum = "/dev/ttyUSB0"
   xbBaudrate = "9600"
   xbNetDiscoveryTimeOut = 10
@@ -15,25 +16,33 @@ class Robot:
     self.xbRemotes = None
 
     self.robotNames = {}
-    for idx in range(0, 1):
+    for idx in range(ROBOTNAME_IDX[0], ROBOTNAME_IDX[1] + 1):
       self.robotNames[str(idx)] = f"Robot {idx}"
   
   def __enter__(self):
     print("[Xbee network]: Initialising..")
     self.xbDevice.open()
     self.xbNet = self.xbDevice.get_network()
-    self.xbNet.set_discovery_timeout(self.xbNetDiscoveryTimeOut)
-    self.xbNet.start_discovery_process()
-    while self.xbNet.is_discovery_running():
-      time.sleep(0.5)
-      print(".")
-    self.xbRemotes = self.xbNet.get_devices()
+    self.updateNetwork()
     print(f"[Xbee Network]: Initialisation finished.")
-    print(f"[Xbee Network]: {len(self.xbRemotes)} remote devices found.")
+    return self
 
   def __exit__(self, _, __, ___):
     self.xbDevice.close()
 
+  def updateNetwork(self, discoveryTimeOut = xbNetDiscoveryTimeOut):
+    if self.xbDevice._is_open():
+      print(f"[Xbee Network]: Network discovery process.")
+      self.xbNet.set_discovery_timeout(self.xbNetDiscoveryTimeOut)
+      self.xbNet.start_discovery_process()
+      while self.xbNet.is_discovery_running():
+        time.sleep(0.5)
+        print(".")
+      self.xbRemotes = self.xbNet.get_devices()
+      print(f"[Xbee Network]: {len(self.xbRemotes)} remote devices found.")
+    else:
+      print(f"[Xbee Network]: Device not open.")
+  
   def sendData(self, data, id = BROADCAST):
     if id == BROADCAST:
       self.xbDevice.send_data_broadcast(data)
