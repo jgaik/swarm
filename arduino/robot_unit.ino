e0f7ab6d9f9ec05139b1b5fdd825b0cb14ecc912
jgaik <jakub.gaik@gmail.com>
2019-09-25 00:33:36 +0200

    Changes: initial reading of data stream for arduino, robot class for controller 	modified:   .vscode/arduino.json 	modified:   .vscode/c_cpp_properties.json 	modified:   arduino/robot_unit.ino 	modified:   robot.py

--- arduino/robot_unit.ino
+++ arduino/robot_unit.ino
@@ -1,4 +1,35 @@
-#include <XBee.h>
+#define BUFFERSIZE 32
 
-void setup(){}
-void loop(){}
+void setValues(String newValues) {
+
+}
+
+void readData() {
+  bool bReadMode = false;
+  char cDataNew[BUFFERSIZE];
+  char cDataStart = '{';
+  char cDataEnd = '}';
+  int indx = 0;
+  while (Serial.available() > 0) {
+    char cRead = Serial.read();
+    if (bReadMode) {
+      if (cRead != cDataEnd) {
+        cDataNew[indx] = cRead;
+        indx++;
+      } else {
+        cDataNew[indx] = '\0';
+        break;
+      }
+    } else if (cRead == cDataStart) {
+      bReadMode = true;
+    }
+  }
+  setValues(String(cDataNew));
+}
+
+void setup() {
+  Serial.begin(9600);
+}
+ 
+void loop() {
+}
--- robot.py
+++ robot.py
@@ -1,37 +1,61 @@
 import time
 import digi.xbee.devices as xb
 
-ROBOTNAME_IDX = (0, 0)
 BROADCAST = -1
+
+class Robot:
+  
+  def __init__(self, addressString, deviceXbee):
+    self.robotAddress = xb.XBee16BitAddress.from_hex_string(addressString)
+    self.device = deviceXbee
+    self.fields = {}
+
+  def getDevice(self):
+    return self.device
+  
+  def getAddress(self):
+    return self.robotAddress
+
+  def addFields(self, fieldEntries):
+    pass
+
+  def getField(self, fieldKey):
+    pass
+
+  @classmethod
+  def createFromList(cls, stringList):
+    pass
+
+  @staticmethod
+  def getDeviceFromList(robotList, addressString):
+    pass
 
 class RobotNetwork:
   xbPortnum = "/dev/ttyUSB0"
   xbBaudrate = "9600"
-  xbNetDiscoveryTimeOut = 10
+  xbNetDiscoveryTimeOut = 5
 
   def __init__(self, portnum = xbPortnum, baudrate = xbBaudrate):
     self.xbBaudrate = baudrate
     self.xbPortnum = portnum
     self.xbDevice = xb.XBeeDevice(self.xbPortnum, self.xbBaudrate)
     self.xbRemotes = None
-
-    self.robotNames = {}
-    for idx in range(ROBOTNAME_IDX[0], ROBOTNAME_IDX[1] + 1):
-      self.robotNames[str(idx)] = f"Robot {idx}"
   
   def __enter__(self):
     print("[Xbee network]: Initialising..")
     self.xbDevice.open()
     self.xbNet = self.xbDevice.get_network()
     self.updateNetwork()
+    self.xbDevice.add_data_received_callback(self.recvDataCallback)
     print(f"[Xbee Network]: Initialisation finished.")
     return self
 
   def __exit__(self, _, __, ___):
+    self.xbDevice.del_data_received_callback(self.recvDataCallback)
     self.xbDevice.close()
 
   def updateNetwork(self, discoveryTimeOut = xbNetDiscoveryTimeOut):
-    if self.xbDevice._is_open():
+    if self.xbDevice._is_open:
       print(f"[Xbee Network]: Network discovery process.")
       self.xbNet.set_discovery_timeout(self.xbNetDiscoveryTimeOut)
       self.xbNet.start_discovery_process()
@@ -43,14 +67,15 @@ class RobotNetwork:
     else:
       print(f"[Xbee Network]: Device not open.")
   
-  def sendData(self, data, id = BROADCAST):
-    if id == BROADCAST:
+  def sendData(self, data, robotAddress = BROADCAST):
+    if robotAddress == BROADCAST:
       self.xbDevice.send_data_broadcast(data)
-    elif str(id) not in self.robotNames:
-      remote = self.xbNet.get_device_by_node_id(self.robotNames[str(id)])
+    else:
+      remote = self.xbNet.get_device_by_16(xb.XBee16BitAddress.from_hex_string(str(robotAddress)))
       if remote is not None:
         self.xbDevice.send_data_async(remote, data)
       else:
-        print(f"[Xbee Network]: {self.robotNames[str(id)]} is not detected.")
-    else:
-      print(f"[Xbee Network]: Unknown id ({id}).")
+        print(f"[Xbee Network]: Unknown robotAddress ({robotAddress}).")
+  
+  def recvDataCallback(self, xbeeMsg):
+    pass
