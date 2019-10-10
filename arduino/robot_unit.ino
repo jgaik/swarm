34f759cca57ddaedcebb31b6bb2f059998d9f37d
jgaik <jakub.gaik@gmail.com>
2019-10-10 19:31:09 +0200

    Changes: message processing v2.0   modified:   arduino/robot_unit.ino

--- arduino/robot_unit.ino
+++ arduino/robot_unit.ino
@@ -19,8 +19,17 @@ int timeDelta = 50;
 #define MSG_END 2
 #define MSG_ERROR 3
 #define HEADER '*'
-int msgStatus = MSG_NONE;
+
+const size_t idxRobotID = 0;
+const size_t idxRobotMode = 1;
+const size_t idxMsgLen = 2;
+const size_t idxParamStart = 3;
+const size_t msgModeLen = 3;
+size_t msgReadCount = 0;
+
+int readStatus = MSG_NONE;
 uint8_t msgBuffer[20] = {};
+
 uint8_t robotMode;
 uint8_t robotStatus;
 
@@ -64,74 +73,53 @@ float convertBytes2Float(uint8_t floatBytes[4]) {
 	return reinterpret_cast<float &>(value32);
 }
 
-bool checkMsg(uint8_t msg[20], size_t msgLen) {
+bool checkMsg() {
+	size_t msgFullLen = msgBuffer[idxMsgLen] + msgModeLen;
 	int modulo = 256;
 	uint8_t sum = 0;
-	for (size_t idx = 0; idx < msgLen - 1; idx++) {
-		sum += msg[idx];
+	for (size_t idx = 0; idx < msgFullLen - 1; idx++) {
+		sum += msgBuffer[idx];
 	}
-	sum = modulo - sum%modulo - 1;
-	return sum == msg[msgLen - 1];
+	sum = modulo - sum - 1;
+	return sum == msgBuffer[msgFullLen - 1];
 }
 
-bool readCommand() {
-	char header = '*';
-	size_t idxRobotID = 0;
-	size_t idxRobotMode = 1;
-	size_t idxMsgLen = 2;
-	size_t idxParamStart = 4;
-
-	size_t commandLen = 3;
-	size_t msgLen = 0;
-
-	bool commandRead = false;
-	bool msgRead = false;
-	bool newCommand = false;
-
-	uint8_t msgBuffer[20];
-	size_t readCount = 0;
-	if (Serial.available() > 0) {
-		Serial.print("[Arduino Robot]: ");
-		Serial.println(Serial.available());
-	}
+void readCommand() {
 	while (Serial.available() > 0) {
 		char readSymbol = Serial.read();
-		Serial.println(readSymbol);
-		if ( (commandRead && readCount < commandLen) || (msgRead && readCount < msgLen) ) {
-			msgBuffer[readCount] = uint8_t(readSymbol);
-			readCount++;
-		} else if (commandRead && readCount >= commandLen) {
-			commandRead = false;
-			msgRead = true;
-			msgLen = msgBuffer[idxMsgLen] + commandLen;
-			Serial.print("[Arduino Robot]: Msg length = ");
-			Serial.println(msgBuffer[idxMsgLen], DEC);
-		} else if (msgRead && readCount >= msgLen) {
-			Serial.print("[Arduino Robot]: Check sum = ");
-			Serial.println(msgBuffer[msgLen - 1], HEX);
+		switch (readStatus) {
+			case MSG_NONE:
+				if (readSymbol == HEADER)
+					readStatus = MSG_READ;
+				break;
+			case MSG_READ:
+				if (msgReadCount < msgBuffer[idxMsgLen] + msgModeLen) {
+					msgBuffer[msgReadCount] = (uint8_t)readSymbol;
+					msgReadCount++;
+				} else {
+					if (checkMsg())
+						readStatus = MSG_END;
+					else
+						readStatus = MSG_ERROR;
+				}
+			break;
+		default:
 			break;
 		}
+	}
+}
 
-		if (!newCommand && readSymbol == header) {
-			Serial.println("[Arduino Robot]: New msg received");
-			newCommand = true;
-			commandRead = true;
-		}
-	}
-	if (newCommand){
-		Serial.print("[Arduino Robot]: Read character count = ");
-		Serial.println(readCount, DEC);
-	}
-	if (newCommand && checkMsg(msgBuffer, msgLen)) {
+void changeState() {
+	if (readStatus == MSG_END) {
 		Serial.println("[Arduino Robot]: New msg is OK");
 		robotMode = msgBuffer[idxRobotMode];
-		for (size_t idx = idxParamStart; idx < msgLen - 1; idx += PARAMETER_BYTE_LEN + 1) {
+		for (size_t idx = 0; idx < msgBuffer[idxMsgLen] - 1; idx += PARAMETER_BYTE_LEN + 1) {
 			for (size_t idxByte = 0; idxByte < PARAMETER_BYTE_LEN; idxByte++) {
-				robotState[(size_t)msgBuffer[idx]][idxByte] = msgBuffer[idx + idxByte + 1];
+				robotState[(size_t)msgBuffer[idx + idxParamStart]][idxByte] = msgBuffer[idx + idxParamStart + idxByte + 1];
 			}
 		}
+		readStatus = MSG_NONE;
 	}
-	return newCommand;
 }
 
 void setup() {
@@ -146,7 +134,7 @@ void setup() {
 }
 
 void loop() {
-	if (readCommand()) {
+	if (readStatus == MSG_END) {
 		Serial.println("[Arduino Robot]: Robot state:");
 		for (size_t idx = 0; idx < ROBOTSTATE_LEN; idx++) {
 			Serial.println(idx);
