a730b6e26e1bb50e92bf2a9754abb28108d26834
jgaik <jakub.gaik@gmail.com>
2020-03-04 17:22:18 +0100

    App with gui and controller thread 	modified:   controller.py 	new file:   gui.py 	modified:   main.py

--- controller.py
+++ controller.py
@@ -4,26 +4,63 @@ import tasks
 import time
 import numpy as np
 
+CODELIST = {
+	'Line': robot.ROBOTPATH_LINE,
+	'Turn': robot.ROBOTPATH_TURN,
+	'Arc': robot.ROBOTPATH_ARC,
+	'Velocity': robot.ROBOTPATH_VELOCITY
+}
+
 def marker():
 	markers = np.array([[0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0], [181.5, 157.5, 0.0, 1.0], [0.0, 0.0, 0.0, 0.0], [208.75, 23.75, -1.8018869939907933, 1.0], [186.5, 119.5, -1.3854483767992019, 1.0], [0.0, 0.0, 0.0, 0.0], [222.0, 163.5, 1.508377516798939, 1.0], [181.0, 200.0, 2.6224465393432705, 1.0], [0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0], [88.75, 75.5, 0.5713374798336268, 1.0], [143.75, 178.25, -0.6947382761967034, 1.0], [128.75, 35.75, 0.540419500270584, 1.0], [0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0], [91.5, 231.25, -2.782821983319221, 1.0], [0.0, 0.0, 0.0, 0.0]])
 	markers[:,0]=markers[:,0]/362*200
-	markers[:,1]=markers[:,1]/298*160
+	markers[:,1]=(298-markers[:,1])/298*160
 	markers[:,0:3]=np.round(markers[:,0:3],3)
-	return markers
+	return markers.tolist()
 
 class Controller():
 
-	def __init__(self):
+	def __init__(self, netPort, netBaudrate, camIp, camPort):
+		self._online = False
 		self._active = False
-		self._swarm = robot.RobotNetwork()
-		self._camera = server.Client()
-		self._markers = marker()
+		self._swarm = robot.RobotNetwork(netPort, netBaudrate)
+		self._camera = server.Client(camIp, camPort)
+		self._markers = {}
+		self._commandReady = False
+		self._commandData = {}
 
 	def run(self):
-		pass
+		with self._swarm:
+			self._online = True
+			while self._active:
+				#self._markers = self._camera.read(server.READ_MARKERS)['markers']
+				if self._commandReady:
+					if self._commandData['task'] == "Direct control":
+						self._commandData['data']['pathMode'] = CODELIST[self._commandData['data']['pathMode']]
+						self._swarm.setRobotVelocity(**self._commandData['data'])
+					if self._commandData['task'] == 'Parameters setting':
+						self._swarm.setRobotParameters(**self._commandData['data'])
+					self._commandReady = False
+
+		self._online = False
 	
 	def getMarkers(self) -> str:
 		return self._markers
+
+	def getIds(self):
+		return self._swarm.robotList.getIds()
+
+	def getIDs(self):
+		if self._online:
+			return self._swarm.robotList.getIds()
+
+	def setData(self, data):
+		if not self._commandReady:
+			self._commandData = data
+			self._commandReady = True
+
+	def isOnline(self):
+		return self._online
 
 	def start(self):
 		self._active = True
--- main.py
+++ main.py
@@ -1,62 +1,55 @@
+import numpy as np
 from controller import Controller
 import tkinter as tk
 from tkinter import ttk
-
-class Gui:
-
-  def __init__(self, master):
-    self.master = master
-
-    self.master.title("Swarm controller")
-
-    self.master.columnconfigure(0, weight=1)
-    self.master.rowconfigure(0, weight=1)
-
-    x = round((self.master.winfo_screenwidth() - self.master.winfo_reqwidth()) / 2)
-    y = round((self.master.winfo_screenheight() - self.master.winfo_reqheight()) / 3)
-    self.master.geometry(f"+{x}+{y}")
-
-    self.master.config(menu=tk.Menu(self.master))
-
-    frame_canvas = ttk.Frame(self)
-    frame_canvas.grid(row=0,column=0, sticky='news')
-
-    self.cnvTable = tk.Canvas(frame_canvas, width=100, height=100)
-    self.cnvTable.grid(row=0,column=0,sticky='news')
-
-    frame_controls = ttk.Frame(self)
-    frame_controls.grid(row=0,column=1,sticky='w')
-
-    self.cmbTasks = ttk.Combobox(frame_controls)
-    self.cmbTasks['values'] = ['Parameters setting', 'Direct control']
-    self.cmbTasks.grid(row=0,column=0,sticky='n')
-
-    frame_settings = ttk.Frame(frame_controls)
-    frame_settings.grid(row=1,column=0,sticky='ew')
-
-    self.btnSend = ttk.Button(frame_controls, text="Send")
-    self.btnSend.grid(row=2, column=0, sticky='se')
-
-  def drawMarkers(self, markersNew):
-    pass
+import gui
+from threading import Thread
 
 class App:
   _time_refresh = 300
 
   def __init__(self, master):
     self.master = master
-    self.gui = Gui(master)
-    self.controller = Controller()
-
+    self.gui = gui.GuiMain(master, self.send)
+    self.controller = None
+    self.th = None
+    self.setup = gui.GuiSetup(master, self.setupEvent, self.end)
+    self.master.withdraw()
     self.master.protocol("WM_DELETE_WINDOW", self.end)
 
   def update(self):
+    if self.controller.isOnline():
+      if (self.master.state() == 'withdrawn'):
+        self.gui.setIds(self.controller.getIds())
+        self.master.deiconify()
+      if (self.setup.status()):
+        self.setup.stop()
+      self.gui.drawMarkers(self.controller.getMarkers())
     self.master.after(self._time_refresh, self.update)
 
   def end(self):
-    self.controller.stop()
+    if self.controller is not None:
+      self.controller.stop()
     self.master.destroy()
 
+  def send(self):
+    data = self.gui.getData()
+    if data:
+      self.controller.setData(data)
+
+  def setupEvent(self):
+    if self.setup.start():
+      self.start()
+
+  def start(self):
+    self.controller = Controller(self.setup.port_num,
+      self.setup.port_baudrate,
+      self.setup.tcp_ip,
+      self.setup.tcp_port)
+    self.th = Thread(target=self.controller.run)
+    self.controller.start()
+    self.th.start()
+    self.update()
 
 if __name__ == "__main__":
   root = tk.Tk()
